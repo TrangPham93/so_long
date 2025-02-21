5afd579b41fac729232a60f70ead813f12d5a4e6
TrangPham93 <hatrangc2@gmail.com>
2025-02-21 14:37:53 +0200

    update gitignore

--- key_input_handler.c
+++ key_input_handler.c
@@ -1,32 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   input_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/18 10:07:57 by trpham            #+#    #+#             */
-/*   Updated: 2025/02/20 20:00:21 by trpham           ###   ########.fr       */
+/*   Created: 2025/02/18 16:45:07 by trpham            #+#    #+#             */
+/*   Updated: 2025/02/18 20:24:57 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(int ac, char **av)
-{
-	t_game	game;
-	t_data	data;
-
-	if (ac != 2)
-		handle_error("No map is chosen");
-	read_map(av[1], &game);
-	data.mlx_ptr = mlx_init();
-	if (!data.mlx_ptr)
-		handle_error("Failed to initialize Mlx");
-	load_window(&data, &game);
-	load_background(&data, &game);
-	load_elements(&data, &game);
-	mlx_loop(data.mlx_ptr);
-	on_destroy(&data);
-	return (0);
-}
