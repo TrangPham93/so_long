3465dde044088b96ec260a1a77c1bbb85a28fd6c
Trang Pham <trpham@c1r5p21.hive.fi>
2025-03-03 17:18:31 +0200

    remove number of movement from screen

--- image_render.c
+++ image_render.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 16:44:35 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/01 17:49:35 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/03 17:15:00 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	render_image(t_data *data)
 	render_img_exit_wall(data);
 	render_img_collectibles(data);
 	render_img_player(data);
-	render_movement_count(data);
+	// render_movement_count(data);
 }
 
 void	render_img_background(t_data *data)
--- player.c
+++ player.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 16:44:51 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/02 11:34:20 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/03 17:16:53 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,25 @@ void	move_player(t_data *data, int new_x, int new_y)
 	data->game->player.x = new_x;
 	data->game->player.y = new_y;
 	data->game->total_move++;
+	ft_printf("Number of movements: %d\n", data->game->total_move);
 	render_image(data);
 }
 
-void	render_movement_count(t_data *data)
-{
-	char	*move_text;
-	int		x_text;
-	int		y_text;
+// void	render_movement_count(t_data *data)
+// {
+// 	char	*move_text;
+// 	int		x_text;
+// 	int		y_text;
 
-	x_text = 10;
-	y_text = 15;
-	move_text = ft_itoa(data->game->total_move);
-	mlx_string_put(data->mlx_ptr, data->mlx_win, x_text, y_text, 0x000000,
-		"Moves: ");
-	mlx_string_put(data->mlx_ptr, data->mlx_win, x_text + 40, y_text, 0x000000,
-		move_text);
-	free(move_text);
-}
+// 	x_text = 10;
+// 	y_text = 15;
+// 	move_text = ft_itoa(data->game->total_move);
+// 	mlx_string_put(data->mlx_ptr, data->mlx_win, x_text, y_text, 0x000000,
+// 		"Moves: ");
+// 	mlx_string_put(data->mlx_ptr, data->mlx_win, x_text + 40, y_text, 0x000000,
+// 		move_text);
+// 	free(move_text);
+// }
 
 void	render_img_player(t_data *data)
 {
