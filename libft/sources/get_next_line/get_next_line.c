792a62733a2f5900383a2bde6d3a66c95813faad
Trang Pham <trpham@c1r6p9.hive.fi>
2025-03-07 15:01:02 +0200

    finish review

--- libft/sources/get_next_line/get_next_line.c
+++ libft/sources/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:32:14 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/04 18:30:23 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/07 14:56:14 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char	*get_next_line(int fd)
 			return (NULL);
 	}
 	if (!update_buffer(&main_buffer, fd))
-		return (free(main_buffer), NULL);
+		return (free(main_buffer), main_buffer = NULL, NULL);
 	if (*main_buffer == '\0')
 		return (free(main_buffer), main_buffer = NULL, NULL);
 	line = extract_line(main_buffer);
